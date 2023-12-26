813eecbc4ead099b27ef15a278c6a979eae237f7
ArpanC0des <arpansilwal@gmail.com>
2023-12-25 22:35:43 -0500

    initial commit with database inside the resourses

---
+++ src/main/java/com/databaseSpringPractice/databasewithSpring/DatabasewithSpringApplication.java
@@ -0,0 +1,14 @@
+package com.databaseSpringPractice.databasewithSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabasewithSpringApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DatabasewithSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/databaseSpringPractice/databasewithSpring/DatabasewithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.databaseSpringPractice.databasewithSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabasewithSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
